050da0a52185c0491888c1e9f5cbcc182769a5fc
nureman <nureman_s@cmu.ac.th>
2023-08-04 16:12:52 +0700

    done no.4

--- vite-project/src/router/index.ts
+++ vite-project/src/router/index.ts
@@ -3,7 +3,12 @@ import EventListView from '../views/EventListView.vue'
 import AboutView from '../views/AboutView.vue'
 import OrganizerView from '../views/OrganizerView.vue'
 import StudentView from '../views/StudentView.vue'
-import EventDetailView from '../views/EventDetailView.vue'
+import EventDetailView from '../views/event/EventDetailView.vue'
+import EventEditView from '../views/event/EventEditView.vue'
+import EventRegisterView from '../views/event/EventRegisterView.vue'
+import EventLayoutView from '@/views/event/EventLayoutView.vue'
+import NotFoundView from '@/views/NotFoundView.vue'
+import NetworkErrorView from '@/views/NetworkErrorView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -34,9 +39,45 @@ const router = createRouter({
     },
     {
       path: '/event/:id',
-      name: 'event-detail',
-      component: EventDetailView,
+      name: 'event-layout',
+      component: EventLayoutView,
+      props: true,
+      children: [
+        {
+          path: '',
+          name: 'event-detail',
+          component: EventDetailView,
+          props: true
+        },
+        {
+          path: 'edit',
+          name: 'event-edit',
+          component: EventEditView,
+          props: true
+        },
+        {
+          path: 'register',
+          name: 'event-register',
+          component: EventRegisterView,
+          props: true
+        }
+      ]
+    },
+    {
+      path: '/:catchAll(.*)',
+      name: 'not-found',
+      component: NotFoundView
+    },
+    {
+      path: '/404/:resource',
+      name: '404-resource',
+      component: NotFoundView,
       props: true
+    },
+    {
+      path: '/network-error',
+      name: 'network-error',
+      component: NetworkErrorView
     }
   ]
 })
