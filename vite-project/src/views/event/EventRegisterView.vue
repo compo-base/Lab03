fc841d82309d725879b1d03ef499119a123db51a
nureman <nureman_s@cmu.ac.th>
2023-08-06 10:29:55 +0700

    fix**

--- vite-project/src/views/event/EventRegisterView.vue
+++ vite-project/src/views/event/EventRegisterView.vue
@@ -9,13 +9,5 @@ defineProps({
 </script>
 
 <template>
-  <div v-if="event">
-    <h1>{{ event.title }}</h1>
-    <div id="nav">
-      <router-link :to="{ name: 'event-detail', params: { id } }">Detail</router-link>
-      <router-link :to="{ name: 'event-register', params: { id } }">Register</router-link>
-      <router-link :to="{ name: 'event-edit', params: { id } }">Edit</router-link>
-    </div>
-    <p>Registration from here</p>
-  </div>
+  <p>Registration from here</p>
 </template>
