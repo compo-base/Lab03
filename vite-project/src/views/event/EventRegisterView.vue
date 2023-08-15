9d12bec724210c517a78fa352370ffa50f5720ee
nureman <nureman_s@cmu.ac.th>
2023-08-15 19:15:01 +0700

    Revert "fix**"
    This reverts commit eadfdfd7690ab2e95638188f81f9d5a14ee44418.

--- vite-project/src/views/event/EventRegisterView.vue
+++ vite-project/src/views/event/EventRegisterView.vue
@@ -9,5 +9,13 @@ defineProps({
 </script>
 
 <template>
-  <p>Registration from here</p>
+  <div v-if="event">
+    <h1>{{ event.title }}</h1>
+    <div id="nav">
+      <router-link :to="{ name: 'event-detail', params: { id } }">Detail</router-link>
+      <router-link :to="{ name: 'event-register', params: { id } }">Register</router-link>
+      <router-link :to="{ name: 'event-edit', params: { id } }">Edit</router-link>
+    </div>
+    <p>Registration from here</p>
+  </div>
 </template>
